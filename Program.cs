069dee2d3f508732fa7c50a1d5b85d91239efb62
Henrique Marques <henrique.marques@epharma.com.br>
2026-03-16 15:06:38 -0300

    feat: serialize and serialize a message from file using ProtoBuf;

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Course.Protobuf.Test;
+
+Console.WriteLine("Welcome to Protobuf Test!");
+var birthDate = new DateTime(1976, 7, 9);
+var birthDateUtc = DateTime.SpecifyKind(birthDate, DateTimeKind.Utc);
+var birthDateTimestamp = Timestamp.FromDateTime(birthDateUtc);
+var employee = new Employee
+{
+    Id = 1,
+    FirstName = "John",
+    LastName = "Wick",
+    IsRetired = false,
+    BirthDate = birthDateTimestamp,
+    MaritalStatus = Employee.Types.MaritalStatus.Married,
+    CurrentAddress = new Address
+    {
+        StreetName = "123 Main St",
+        City = "Anytown",
+        ZipCode = "12345",
+    },
+    PreviousEmployers = { "Google", "Microsoft", "Apple" },
+};
+
+// Serialize the employee object to a file
+using (var output = File.Create("emp.dat"))
+{
+    employee.WriteTo(output);
+}
+
+// Deserialize the employee object from the file
+Employee empFromFile;
+using (var input = File.OpenRead("emp.dat"))
+{
+    empFromFile = Employee.Parser.ParseFrom(input);
+}
+
+Console.WriteLine(empFromFile.ToString());
+
+Console.WriteLine("Finished!");
